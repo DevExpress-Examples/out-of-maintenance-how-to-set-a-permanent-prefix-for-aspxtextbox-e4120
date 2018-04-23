cc9223bdca6b67f5ee4866353653cf03860be6e1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using DevExpress.Web.ASPxEditors;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void Page_Load(object sender, EventArgs e) {
+    }
+    protected void testingTextBox_Init(object sender, EventArgs e) {
+        ASPxTextBox tb = sender as ASPxTextBox;
+        string script = "function (s,e) { OnKeyDown(e.htmlEvent,s.GetInputElement(),s); }";
+        if(((HttpCapabilitiesBase)(Request.Browser)).Browser == "Opera") 
+            tb.ClientSideEvents.KeyPress = script;
+        else
+            tb.ClientSideEvents.KeyDown = script;
+        
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to set a permanent prefix for ASPxTextBox
+
+
+<p>This example demonstrates how to set a permanent prefix for ASPxTextBox using javascript.<br />
+Note that the keydown event works to prevent the default action in all browsers except Opera. So, it is  necessary to handle the keypress here.</p>
+
+<br/>
+
+
