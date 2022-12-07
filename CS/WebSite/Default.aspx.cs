5540c3710334055eac799feec104445f24ea2bf0
DevExpressExampleBot <fakeEmail>
2022-12-07 14:32:06 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.Configuration;
-using DevExpress.Web.ASPxEditors;
+using DevExpress.Web;
 
 public partial class _Default : System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -6,7 +6,7 @@ Imports System.Web
 Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports System.Web.Configuration
-Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web
 
 Partial Public Class _Default
 	Inherits System.Web.UI.Page
